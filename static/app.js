4c8f17f0952ef0e26849499264f0b9bbde15a118
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-03-12 17:05:27 -0600

    updated index to display guess and the guess result

--- static/app.js
+++ static/app.js
@@ -1,12 +1,13 @@
 let $submit = $('#submitBtn');
-let $message = $('.guessMessage');
+let $messageList = $('.guessMessages');
 
 async function submitGuess(e) {
     e.preventDefault();
 
     let guess = $('#guess').val();
     let response = await axios.get('/guess', { params: { guess: guess } });
-    console.log(response);
+    let phrase = `<p class='phrase'>${guess} is ${response.data.result}</p>`
+    $messageList.append(phrase)
 }
 
 
