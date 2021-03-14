59388773b951be93caec9d2ca946ce2adb1c9342
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-03-14 17:37:28 -0500

    keeping track of high score and number of times played on back end

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from boggle import Boggle
 from flask import Flask, request, render_template, redirect, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+import pdb
 
 boggle_game = Boggle()
 app = Flask(__name__)
 
 app.config['SECRET_KEY']="oh-so-secret"
 debug = DebugToolbarExtension(app)
+
 
 @app.route('/')
 def home_page():
@@ -21,3 +23,12 @@ def make_guess():
     result = boggle_game.check_valid_word(board,word)
     return jsonify({'result': result})
 
+@app.route('/session', methods=['POST'])
+def update_session():
+    total = request.json["total"]
+    highscore = session.get("highscore", 0)
+    times = session.get("num_times_played", 0)
+
+    session['num_times_played'] = times + 1
+    session['highscore'] = max(total, highscore)
+    return jsonify({'highscore': session['highscore'] })
--- static/app.js
+++ static/app.js
@@ -1,30 +1,45 @@
 let $submit = $('#submitBtn');
 let $messageList = $('.guessMessages');
 let $score = $('#score');
-let $guessForm = $('#guessForm')
+let $guessForm = $('#guessForm');
+let $highScore = $('#highScore');
 let total = 0;
 let numGuesses = 0;
+let guesses = [];
 async function submitGuess(e) {
     e.preventDefault();
     if (numGuesses === 0) {
-        setTimeout(removeForm, 60000);
+        setTimeout(removeForm, 10000);
     }
     numGuesses += 1;
     let guess = $('#guess').val();
-    let response = await axios.get('/guess', { params: { guess: guess } });
-    let phrase = `<p class='phrase'>${guess} is ${response.data.result}</p>`
-    $messageList.append(phrase);
-    if (response.data.result === 'ok') {
-        total += guess.length;
-        $score.text(`Total score: ${total}`);
+    if (guesses.indexOf(guess) === -1) {
+        guesses.push(guess);
+        let response = await axios.get('/guess', { params: { guess: guess } });
+        let phrase = `<p class='phrase'>${guess} is ${response.data.result}</p>`;
+        $messageList.prepend(phrase);
+        if (response.data.result === 'ok') {
+            total += guess.length;
+            $score.text(`Total score: ${total}`);
+        }
+    } else {
+        let phrase = `<p class='phrase'>You already guessed ${guess}!</p>`;
+        $messageList.prepend(phrase)
     }
 }
 
 
 $submit.on('click', submitGuess);
 
-function removeForm() {
+async function removeForm() {
     $guessForm.remove();
     $messageList.remove();
-    $score.text(`Final score: ${total}`)
+    $score.text(`Final score: ${total}`);
+    await updateSession();
+}
+
+async function updateSession() {
+    let response = await axios.post('/session', { total: total });
+    console.log(response);
+    $highScore.text(`High Score: ${response.data.highscore}`)
 }
