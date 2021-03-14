3613e28b82a4d96cb9577f796b1a017aa269faa3
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-03-14 10:52:56 -0500

    added the score tally to DOM, and removes form after 60 secs

--- static/app.js
+++ static/app.js
@@ -1,14 +1,30 @@
 let $submit = $('#submitBtn');
 let $messageList = $('.guessMessages');
-
+let $score = $('#score');
+let $guessForm = $('#guessForm')
+let total = 0;
+let numGuesses = 0;
 async function submitGuess(e) {
     e.preventDefault();
-
+    if (numGuesses === 0) {
+        setTimeout(removeForm, 60000);
+    }
+    numGuesses += 1;
     let guess = $('#guess').val();
     let response = await axios.get('/guess', { params: { guess: guess } });
     let phrase = `<p class='phrase'>${guess} is ${response.data.result}</p>`
-    $messageList.append(phrase)
+    $messageList.append(phrase);
+    if (response.data.result === 'ok') {
+        total += guess.length;
+        $score.text(`Total score: ${total}`);
+    }
 }
 
 
 $submit.on('click', submitGuess);
+
+function removeForm() {
+    $guessForm.remove();
+    $messageList.remove();
+    $score.text(`Final score: ${total}`)
+}
