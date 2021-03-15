d6d6d7213262f1ca19c3b5b5dd4fe9b47e00220f
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-03-14 22:42:01 -0500

    made the boggle class in js file

--- static/app.js
+++ static/app.js
@@ -1,60 +1,64 @@
-let $submit = $('#submitBtn');
-let $messageList = $('.guessMessages');
-let $score = $('#score');
-let $guessForm = $('#guessForm');
-let $highScore = $('#highScore');
-let $timeCounter = $('#timeCounter');
-let total = 0;
-let numGuesses = 0;
-let guesses = [];
-let secs = 60;
-
-async function submitGuess(e) {
-    e.preventDefault();
-    if (numGuesses === 0) {
-        setTimeout(removeForm, 60000);
-        setInterval(() => {
-            countdown();
-        }, 1000);
+class Game {
+    constructor() {
+        this.board = $('#boggle');
+        this.timeCounter = $('#timeCounter');
+        this.messageList = $('.guessMessages');
+        this.score = $('#score');
+        this.guessForm = $('#guessForm');
+        this.highScore = $('#highScore');
+        this.total = 0;
+        this.numGuesses = 0;
+        this.guesses = [];
+        this.secs = 10;
+        $('#submitBtn', this.board).on('click', this.submitGuess.bind(this));
     }
-    numGuesses += 1;
-    let guess = $('#guess').val();
-    if (guesses.indexOf(guess) === -1) {
-        guesses.push(guess);
-        let response = await axios.get('/guess', { params: { guess: guess } });
-        let phrase = `<p class='phrase'>${guess} is ${response.data.result}</p>`;
-        $messageList.prepend(phrase);
 
-        if (response.data.result === 'ok') {
-            total += guess.length;
-            $score.text(`Total score: ${total}`);
+    async submitGuess(e) {
+        e.preventDefault();
+        if (this.numGuesses === 0) {
+            setTimeout(this.removeForm.bind(this), this.secs * 1000);
+            setInterval(() => {
+                this.countdown();
+            }, 1000);
         }
-    } else {
-        let phrase = `<p class='phrase'>You already guessed ${guess}!</p>`;
-        $messageList.prepend(phrase)
+        this.numGuesses += 1;
+        let guess = $('#guess').val();
+        if (this.guesses.indexOf(guess) === -1) {
+            this.guesses.push(guess);
+            let response = await axios.get('/guess', { params: { guess: guess } });
+            let phrase = `<p class='phrase'>${guess} is ${response.data.result}</p>`;
+            this.messageList.prepend(phrase);
+
+            if (response.data.result === 'ok') {
+                this.total += guess.length;
+                this.score.text(`Total score: ${this.total}`);
+            }
+        } else {
+            let phrase = `<p class='phrase'>You already guessed ${guess}!</p>`;
+            this.messageList.prepend(phrase)
+        }
+    }
+    countdown() {
+        this.secs -= 1
+        this.timeCounter.text(`Time remaining: ${this.secs} seconds`);
+        if (this.secs === 0) {
+            this.timeCounter.remove()
+        }
+    }
+    async removeForm() {
+        await this.updateSession();
+        $('#guessForm').remove();
+        this.messageList.remove();
+        this.score.text(`Final score: ${this.total}`);
+    }
+
+    async updateSession() {
+        let response = await axios.post('/session', { total: this.total });
+
+        this.highScore.text(`High Score: ${response.data.highscore}`)
     }
 }
 
 
-$submit.on('click', submitGuess);
+let boggle = new Game();
 
-function countdown() {
-    secs -= 1
-    $timeCounter.text(`Time remaining: ${secs} seconds`);
-    if (secs === 0) {
-        $timeCounter.remove()
-    }
-}
-
-async function removeForm() {
-    $guessForm.remove();
-    $messageList.remove();
-    $score.text(`Final score: ${total}`);
-    await updateSession();
-}
-
-async function updateSession() {
-    let response = await axios.post('/session', { total: total });
-
-    $highScore.text(`High Score: ${response.data.highscore}`)
-}
