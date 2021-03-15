498775cdeffc650e7172a0f690abd35c9b7ee642
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-03-15 11:55:33 -0500

    wrote tests for routes

--- test.py
+++ test.py
@@ -9,5 +9,29 @@ app.config['DEBUG_TB_HOSTS']=['dont-show-debug-toolbar']
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def test_home_page(self):
+        '''test the home route to make sure the board is saved in session, and html is displayed'''
+        with app.test_client() as client:
+            
+            resp = client.get('/')
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<h1>Play Boggle!</h1>', html)
+            self.assertIn('board', session)
 
+    def test_guess_page(self):
+        '''test if guess request evaluates word correctly'''
+        with app.test_client() as client:
+                client.get('/')
+                resp = client.get('/guess?guess=skdfjskfjk')
+                self.assertEqual(resp.json['result'],'not-word')
+
+    def test_session(self):
+        '''test to see if a score is stored as the highscore in a session'''
+        with app.test_client() as client:
+            response = client.post('/session', json={ 'total' : 10 })
+            client.get("/")
+            response = client.post('/session', json={ 'total' : 20 })
+            client.get("/")
+            self.assertEqual(session['highscore'], 20)
+                
