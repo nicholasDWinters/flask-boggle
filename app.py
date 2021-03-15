a62f9dd3fb285db263ce91e9bb68abefacf3ae2a
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-03-14 21:27:08 -0500

    added timer functionality

--- app.py
+++ app.py
@@ -12,12 +12,14 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def home_page():
+    '''render home page, remember the board in the session'''
     board = boggle_game.make_board()
     session['board'] = board
     return render_template('index.html', board=board)
 
 @app.route('/guess')
 def make_guess():
+    '''take the user's guess, and run the check_valid_word function on it, return the result'''
     board = session['board']
     word = request.args['guess']
     result = boggle_game.check_valid_word(board,word)
@@ -25,10 +27,13 @@ def make_guess():
 
 @app.route('/session', methods=['POST'])
 def update_session():
+    '''at the end of the round, grab the total score from the DOM, set the highscore and number of times session variables and update them, 
+    and return the highscore session variable to update the DOM'''
     total = request.json["total"]
     highscore = session.get("highscore", 0)
     times = session.get("num_times_played", 0)
 
     session['num_times_played'] = times + 1
     session['highscore'] = max(total, highscore)
+    
     return jsonify({'highscore': session['highscore'] })
--- static/app.js
+++ static/app.js
@@ -3,13 +3,19 @@ let $messageList = $('.guessMessages');
 let $score = $('#score');
 let $guessForm = $('#guessForm');
 let $highScore = $('#highScore');
+let $timeCounter = $('#timeCounter');
 let total = 0;
 let numGuesses = 0;
 let guesses = [];
+let secs = 60;
+
 async function submitGuess(e) {
     e.preventDefault();
     if (numGuesses === 0) {
-        setTimeout(removeForm, 10000);
+        setTimeout(removeForm, 60000);
+        setInterval(() => {
+            countdown();
+        }, 1000);
     }
     numGuesses += 1;
     let guess = $('#guess').val();
@@ -18,6 +24,7 @@ async function submitGuess(e) {
         let response = await axios.get('/guess', { params: { guess: guess } });
         let phrase = `<p class='phrase'>${guess} is ${response.data.result}</p>`;
         $messageList.prepend(phrase);
+
         if (response.data.result === 'ok') {
             total += guess.length;
             $score.text(`Total score: ${total}`);
@@ -31,6 +38,14 @@ async function submitGuess(e) {
 
 $submit.on('click', submitGuess);
 
+function countdown() {
+    secs -= 1
+    $timeCounter.text(`Time remaining: ${secs} seconds`);
+    if (secs === 0) {
+        $timeCounter.remove()
+    }
+}
+
 async function removeForm() {
     $guessForm.remove();
     $messageList.remove();
@@ -40,6 +55,6 @@ async function removeForm() {
 
 async function updateSession() {
     let response = await axios.post('/session', { total: total });
-    console.log(response);
+
     $highScore.text(`High Score: ${response.data.highscore}`)
 }
